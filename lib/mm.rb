674c570f5b59f1ecebab99b78fc3dd32e37c6371
Alejaldo <statfluct@yahoo.com>
2020-11-20 10:14:47 +0500

    Correcting mm.rb

--- lib/mm.rb
+++ lib/mm.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require './lib/mm/settings_module.rb'
-require './lib/mm/board_class.rb'
-require './lib/mm/player_class.rb'
-require './lib/mm/playerAI_class.rb'
-require './lib/mm/game_class.rb'
+require './mm/settings_module.rb'
+require './mm/board_class.rb'
+require './mm/player_class.rb'
+require './mm/playerAI_class.rb'
+require './mm/game_class.rb'
 include Settings
 
 mastermind_game = Game.new
